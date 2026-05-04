a54bb1655e028d21f51d7743583a04dbcc76bc57
Yi <wy7382@gmail.com>
2026-05-04 19:05:26 +0930

    feat: scrape on startup then every 24h via cron

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
+import { Cron } from 'croner'
 import { statsRoutes } from './api/stats'
+import { runScrape } from './scrapers'
 
 export const app = new Elysia()
   .use(cors())
@@ -10,4 +12,7 @@ if (import.meta.main) {
   app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Server running on port ${app.server?.port}`)
   })
+
+  runScrape()
+  new Cron('0 0 * * *', runScrape)
 }
