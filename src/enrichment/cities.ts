cb2ac14bee4d2e0b1e7b313caef4d7359bba9eb4
Yi <wy7382@gmail.com>
2026-05-04 21:11:00 +0930

    feat: expand city list to cover all major Australian cities

--- src/enrichment/cities.ts
+++ src/enrichment/cities.ts
@@ -1,7 +1,22 @@
 const CITIES = [
+  // Major metros
   'Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Adelaide',
-  'Canberra', 'Darwin', 'Hobart', 'Gold Coast', 'Newcastle',
-  'Wollongong', 'Sunshine Coast', 'Geelong', 'Townsville', 'Cairns',
+  'Canberra', 'Darwin', 'Hobart',
+  // Queensland
+  'Gold Coast', 'Sunshine Coast', 'Townsville', 'Cairns', 'Toowoomba',
+  'Mackay', 'Rockhampton', 'Bundaberg', 'Hervey Bay', 'Gladstone',
+  // NSW
+  'Newcastle', 'Wollongong', 'Central Coast', 'Maitland', 'Wagga Wagga',
+  'Albury', 'Orange', 'Dubbo', 'Lismore', 'Ballina', 'Port Macquarie',
+  // Victoria
+  'Geelong', 'Ballarat', 'Bendigo', 'Shepparton', 'Mildura',
+  'Wodonga', 'Warrnambool', 'Traralgon',
+  // WA
+  'Mandurah', 'Bunbury', 'Geraldton', 'Kalgoorlie', 'Albany',
+  // SA
+  'Mount Gambier', 'Whyalla', 'Murray Bridge',
+  // Tasmania
+  'Launceston', 'Devonport', 'Burnie',
 ]
 
 const CITY_PATTERN = new RegExp(CITIES.join('|'), 'i')
